087e0bfa816b32036867f46cc27068a07f4a7b7a
Sellucas <lucassellmachado@gmail.com>
2023-08-29 20:22:46 -0300

    fix: logo design

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around p-24 bg-[#1E2749]">
       <div className="radial" />
-      <h1 className="text-7xl font-bold text-[#FAFAFF]">CluedUp.</h1>
-      <Button
-        size={'lg'}
-        className="text-xl shadow-xl flex items-center justify-center"
-      >
+      <h1 className="text-7xl font-bold text-[#FAFAFF] tracking-tighter">
+        CluedUp.
+      </h1>
+      <Button className="text-lg shadow-xl flex flex-col md:flex-row items-center justify-center h-15">
         Under Construction
         <span>
           <RotateCw className="animate-spin ml-2" absoluteStrokeWidth />
