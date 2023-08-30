885a574b9757d18fbb4cad5cb9df18c0626a4fe9
Sellucas <lucassellmachado@gmail.com>
2023-08-30 12:07:09 -0300

    feat: update title and button

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-around p-24 bg-[#1E2749]">
+    <main className="flex min-h-screen flex-col items-center justify-around p-12 bg-[#1E2749]">
       <div className="radial" />
-      <h1 className="text-7xl font-bold text-[#FAFAFF] tracking-tighter">
-        CluedUp.
+      <h1 className="text-4xl md:text-6xl max-w-4xl font-bold text-center text-[#FAFAFF] tracking-tight">
+        Gerencie de forma eficiente seus estudos, projetos, acompanhe suas
+        candidaturas e eventos
       </h1>
-      <Button className="text-lg shadow-xl flex flex-col md:flex-row items-center justify-center h-15">
-        Under Construction
+      <Button className="text-lg shadow-xl flex flex-row items-center justify-center h-15">
+        Em construção
         <span>
           <RotateCw className="animate-spin ml-2" absoluteStrokeWidth />
         </span>
