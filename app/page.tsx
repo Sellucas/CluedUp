45061241e8145d8a248338b44ee0c997bc9889ea
Sellucas <lucassellmachado@gmail.com>
2023-08-30 13:39:52 -0300

    feat: update design

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around p-12 bg-[#1E2749]">
       <div className="radial z-10" />
-      <div className="flex flex-col justify-center items-center text-center">
+      <div className="flex flex-col justify-center items-center text-center mt-12">
         <h1 className="text-4xl md:text-7xl max-w-4xl font-bold text-[#FAFAFF] tracking-tight">
           Gerencie de forma eficiente seus estudos, projetos, acompanhe suas
           candidaturas e eventos
