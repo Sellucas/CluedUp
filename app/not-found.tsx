27f63d44a903b9c993086e8a0a44abac3c1859c5
Sellucas <lucassellmachado@gmail.com>
2023-09-01 15:20:38 -0300

    feat: add 404 not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,52 @@
+import { MoveLeft } from 'lucide-react'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <>
+      <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
+        <div className="flex flex-col items-center max-w-sm mx-auto text-center">
+          <p className="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 dark:bg-gray-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+              />
+            </svg>
+          </p>
+          <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-4xl">
+            Página não encontrada
+          </h1>
+          <p className="mt-4 text-xl text-gray-500 dark:text-gray-400">
+            A página que você procura não existe. Aqui estão alguns links úteis:
+          </p>
+
+          <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+            <Link
+              href={'/'}
+              className="flex items-center text-xl justify-center w-1/2 px-5 py-2 text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700"
+            >
+              <MoveLeft />
+              Voltar
+            </Link>
+
+            <Link
+              href={'/'}
+              className="w-1/2 px-5 py-2 text-xl tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600"
+            >
+              Ir para Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
